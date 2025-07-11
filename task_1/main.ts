730a2f52711690e6e1e16a946e2dfaa0aa0f5a47
Mokete <ematyabekana@gmail.com>
2025-07-11 12:11:13 +0200

    Created a teacher interface.

---
+++ task_1/main.ts
@@ -0,0 +1,12 @@
+// This is an interface for a TypeScript project that uses Webpack, ESLint, and TypeScript configuration files. The code includes comments indicating the purpose of each file and the changes made to them.
+
+
+interface Teacher{
+    firstName: string;
+    lastName: string;
+    yearsOfExperience: number;
+    fullTimeEmployee: boolean;
+    location: string;
+    contract:boolean;
+}
+
