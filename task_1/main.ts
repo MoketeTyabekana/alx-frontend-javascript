3fb4d1a2aeeb4fac72beccdba3810a90cf73ad83
Mokete <ematyabekana@gmail.com>
2025-07-11 12:42:59 +0200

    Created  a teacher interface, and a directors interface that extents the teachers interface.

--- task_1/main.ts
+++ task_1/main.ts
@@ -10,3 +10,39 @@ interface Teacher{
     contract:boolean;
 }
 
+const teacher1: Teacher = {
+    firstName: "Mokete",
+    lastName: "Tyabekana",
+    yearsOfExperience: 4,
+    fullTimeEmployee: true,
+    location: "Midrand",
+    contract: false,
+}
+
+console.log(teacher1);
+
+
+interface Directors extends Teacher {
+    numberOfReports: number; 
+}
+
+const director1: Directors = {
+    firstName: "Mpho",
+    lastName: "Tyabekana",
+    yearsOfExperience: 2,
+    fullTimeEmployee: false,
+    location: "Bloemfontein",
+    contract: false,
+    numberOfReports: 5,
+}
+
+
+
+function printTeacher(firstName: string, lastName: string): string {
+    return `${firstName.charAt(0)}. ${lastName}`;
+
+}
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;  
+}
