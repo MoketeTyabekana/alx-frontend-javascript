eaec7f8fce08809019e187c25e6ee942404c1ccd
Mokete <ematyabekana@gmail.com>
2025-07-11 11:42:19 +0200

    Created a Student interface, then added two students and created an arralist that takes the two students.

---
+++ task_0/main.ts
@@ -0,0 +1,25 @@
+// This is an interface for a TypeScript project that uses Webpack, ESLint, and TypeScript configuration files. The code includes comments indicating the purpose of each file and the changes made to them.
+
+
+interface Student{
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string;
+}
+
+const student1: Student = {
+    firstName: "Mokete",
+    lastName: "Tyabekana",
+    age: 31,
+    location: "Midrand"
+};
+
+const student2: Student = {
+    firstName: "Mpho",
+    lastName: "Oganne",
+    age: 27,
+    location: "Randburg"
+};
+
+const studentsList: Student[] = [student1, student2];
