3038f38447bf416866b19ec80510c63f0396d9f2
Mokete <ematyabekana@gmail.com>
2025-07-13 13:06:12 +0200

    imported the RowID and RowElement from interface.js, the declared a namespace CRUD to group the function declaration.

--- task_3/js/crud.d.ts
+++ task_3/js/crud.d.ts
@@ -1,3 +1,8 @@
-import RowID from './interface';
-import RowElement from './interface';
+import { RowID, RowElement } from "./interface";
 
+declare namespace CRUD {
+  function create(row: RowElement): RowID;
+  function read(id: RowID): RowElement | null;
+  function update(row: RowID & Partial<RowElement>): RowElement;
+  function deleteRow(id: RowID): void;
+}
